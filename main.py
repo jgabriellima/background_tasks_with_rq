4bd7a06afc2443c1a0c57317974a4a117a863acf
João Gabriel Lima <joagabriellima@MacBook-Pro-Joao-Gabriel-Lima.local>
2022-05-30 13:35:34 -0300

    Seg 30 Mai 2022 13:35:34 -03

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from worker import conn, q
+from worker import q
 from utils import count_words_at_url
 
 app = FastAPI()
@@ -9,4 +9,11 @@ app = FastAPI()
 async def root():
     source = 'http://heroku.com'
     job = q.enqueue(count_words_at_url, source)
+    return {"message": f"source: {source} result: {job}"}
+
+
+@app.get("/job/:id")
+async def readjob(id: str):
+    source = 'http://heroku.com'
+    job = q.fetch_job(id)
     return {"message": f"source: {source} result: {job.result}"}
